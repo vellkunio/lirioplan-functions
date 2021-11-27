dd515cb3819247265faa33b246374cb74ebcadb4
Serhii <batestaaa13@gmail.com>
2021-11-27 16:16:27 -0500

    done api for materials

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,13 @@ const {
     getAuthenticatedUser
 } = require('./handlers/users');
 
+const {
+    addMaterial,
+    getAllMaterials,
+    editMaterial,
+    deleteMaterial
+} = require('./handlers/materials');
+
 const functions = require("firebase-functions");
 const app = require('express')();
 
@@ -26,6 +33,12 @@ app.get('/projects', getAllProjects);
 app.post('/addProject', FBAuth, addProject);
 app.post('/editProject/:projectId', FBAuth, editProject);
 app.delete('/project/:projectId', FBAuth, deleteProject);
+
+//Materials Routes
+app.post('/addMaterial', FBAuth, addMaterial);
+app.get('/materials', FBAuth, getAllMaterials);
+app.post('/editMaterial/:materialId', FBAuth, editMaterial);
+app.delete('/material/:materialId', FBAuth, deleteMaterial);
 
 //SIGNUP ROUTES
 app.post('/signup', signup);
