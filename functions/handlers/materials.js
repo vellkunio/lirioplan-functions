8ad9c20395f0020a403daa29719fb387f3a35c71
Serhii <batestaaa13@gmail.com>
2022-02-10 15:35:53 -0500

    ...

--- functions/handlers/materials.js
+++ functions/handlers/materials.js
@@ -1,7 +1,17 @@
 const { db } = require('../util/admin');
 const { updateMaterialDetails} = require('../util/validators');
 
+function round(num) {
+    var m = Number((Math.abs(num) * 100).toPrecision(15));
+    return Math.round(m) / 100 * Math.sign(num);
+}
+
 exports.addMaterial = (req, res) => {
+
+    let sqftPerTile = (req.body.length/12) * (req.body.width/12);
+    let sqftPerBox = (req.body.quantityInBox) * sqftPerTile;
+    sqftPerTile = round(sqftPerTile);
+    sqftPerBox = round(sqftPerBox);
    
     const newMaterial = {
         // createdAt: admin.firestore.Timestamp.now(), //Will show date in seconds like "_seconds": 1634864349, "_nanoseconds": 420000000
@@ -15,13 +25,15 @@ exports.addMaterial = (req, res) => {
         length: req.body.length,
         width: req.body.width,
         height: req.body.height,
-        sqftPerTile: req.body.sqftPerTile,
-        sqftPerBox: req.body.sqftPerBox,
+        sqftPerTile,                        //changable
+        sqftPerBox,                         //changable
         price: req.body.price,
         quantity: req.body.quantity
     };
+    
+    
 
-    // if(req.user.isAdmin){
+    if(req.user.userStatus === 'admin'){
     db.collection('materials').add(newMaterial)
     .then(doc => {
         res.json({ message: `document ${doc.id} was successully created!`});
@@ -30,9 +42,9 @@ exports.addMaterial = (req, res) => {
         res.status(500).json({error: 'something went wrong'});
         console.error(err);
     });
-    // } else {
-    //     return res.status(403).json({ general: "Only administrators are allowed to do so" });
-    // }
+    } else {
+        return res.status(403).json({ general: "Only administrators are allowed to do so" });
+    }
 };
 
 exports.getAllMaterials = (req, res) => {
@@ -62,7 +74,7 @@ exports.editMaterial = (req, res) => {
     const documentRoute = db.collection('materials').doc(req.params.materialId);
     let updatedMaterialData = updateMaterialDetails(req.body);
 
-    // if(req.user.isAdmin){
+    if(req.user.userStatus === 'admin'){
         documentRoute.update(updatedMaterialData)
             .then(() => {
                 return res.json({ message: "Edited successfully" });
@@ -71,10 +83,10 @@ exports.editMaterial = (req, res) => {
                 console.error(err);
                 return res.status(500).json({ error: err.code });
             });
-    // }
-    // else {
-    //     return res.status(403).json({ general: "Only administrators are allowed to do so" });
-    // }
+    }
+    else {
+        return res.status(403).json({ general: "Only administrators are allowed to do so" });
+    }
 
     
 }
@@ -88,14 +100,14 @@ exports.deleteMaterial = (req, res) => {
         if(!doc.exists){
             return res.status(404).json({ error: 'Material not found' });
         }
-        // if(!req.user.isAdmin){
-        //     return res.status(403).json({ error: 'Unauthorized'})
-        // } else {
+        if(req.user.userStatus != 'admin'){
+            return res.status(403).json({ error: 'Unauthorized'})
+        } else {
             return document.delete();
-        // }
+        }
     })
     .then(()=>{
-        res.json({ message: 'Material deleted successfully'});
+        res.json({ message: `Material deleted successfully`});
     })
     .catch(err=>{
         console.error(err);
